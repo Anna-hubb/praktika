b73a0d909f13c89e463ec7a6fd89afeac84db7f8
Anna-hubb <160769056+Anna-hubb@users.noreply.github.com>
2024-06-24 22:44:42 +0300

    Add files via upload

---
+++ src/views/data.js
@@ -0,0 +1,122 @@
+export const Data = [
+    {
+        date: "2024-05-16",
+        visitors: 6,
+        returning_visitors: 5,
+        average_visit_duration: 590
+    },
+    {
+        date: "2024-05-17",
+        visitors: 32,
+        returning_visitors: 7,
+        average_visit_duration: 149
+    },
+    {
+        date: "2024-05-18",
+        visitors: 19,
+        returning_visitors: 10,
+        average_visit_duration: 368
+    },
+    {
+        date: "2024-05-19",
+        visitors: 27,
+        returning_visitors: 19,
+        average_visit_duration: 329
+    },
+    {
+        date: "2024-05-20",
+        visitors: 27,
+        returning_visitors: 20,
+        average_visit_duration: 79
+    },
+    {
+        date: "2024-05-21",
+        visitors: 24,
+        returning_visitors: 16,
+        average_visit_duration: 531
+    },
+    {
+        date: "2024-05-22",
+        visitors: 31,
+        returning_visitors: 22,
+        average_visit_duration: 412
+    },
+    {
+        date: "2024-05-23",
+        visitors: 30,
+        returning_visitors: 16,
+        average_visit_duration: 558
+    },
+    {
+        date: "2024-05-24",
+        visitors: 2,
+        returning_visitors: 2,
+        average_visit_duration: 37
+    },
+    {
+        date: "2024-05-25",
+        visitors: 40,
+        returning_visitors: 15,
+        average_visit_duration: 323
+    },
+    {
+        date: "2024-05-26",
+        visitors: 46,
+        returning_visitors: 15,
+        average_visit_duration: 398
+    },
+    {
+        date: "2024-05-27",
+        visitors: 19,
+        returning_visitors: 4,
+        average_visit_duration: 25
+    },
+    {
+        date: "2024-05-28",
+        visitors: 7,
+        returning_visitors: 3,
+        average_visit_duration: 417
+    },
+    {
+        date: "2024-05-29",
+        visitors: 16,
+        returning_visitors: 11,
+        average_visit_duration: 284
+    },
+    {
+        date: "2024-05-30",
+        visitors: 11,
+        returning_visitors: 10,
+        average_visit_duration: 122
+    },
+    {
+        date: "2024-05-31",
+        visitors: 48,
+        returning_visitors: 18,
+        average_visit_duration: 492
+    },
+    {
+        date: "2024-06-01",
+        visitors: 29,
+        returning_visitors: 11,
+        average_visit_duration: 592
+    },
+    {
+        date: "2024-06-02",
+        visitors: 42,
+        returning_visitors: 13,
+        average_visit_duration: 524
+    },
+    {
+        date: "2024-06-03",
+        visitors: 22,
+        returning_visitors: 7,
+        average_visit_duration: 104
+    },
+    {
+        date: "2024-06-04",
+        visitors: 41,
+        returning_visitors: 19,
+        average_visit_duration: 42
+    }
+]
